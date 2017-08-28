daff6a84cfe4fb290cf6753295a23844470ae8c0
ASPP Student <student@localhost>
2017-08-28 18:17:20 +0300

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/alebyk113/aspp2017.git',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
